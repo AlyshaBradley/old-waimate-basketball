8c13a21c7a7bae69d3e8c4ba481ef9d5e37cf119
AlyshaBradley <bradleal02@craighead.school.nz>
2021-05-18 18:15:39 +1200

    index page
    ADDED:
    - added columns to index page
    - created three seperate columns, middle one specifically for the draw.
    - added a fake draw as an image to get an idea, will replace with a real draw later etc.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,30 +4,85 @@
 
 <section class="hero is-info is-fullheight-with-navbar">
   <div class="hero-body">
-    <p class="title">
-      waimate bball
-    </p>
+    <p class="title">waimate bball</p>
   </div>
 </section>
 
 <Nav />
+<div class="columns">
+<div class="column is-one-quarter">
+  <div class="box">
+    <h1>Text</h1>
+    <hr>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+      sed velit sit amet felis eleifend finibus. Maecenas eu nisl sit amet
+      augue vehicula ultricies. Class aptent taciti sociosqu ad litora
+      torquent per conubia nostra, per inceptos himenaeos. Morbi sit amet
+      lectus at augue vulputate ultrices. Ut volutpat ullamcorper vulputate.
+      Praesent et orci quis nisi consequat eleifend ut sed purus. Donec quis
+      feugiat leo. Ut in posuere velit, sed iaculis augue. Ut vitae libero nec
+      ex posuere facilisis. Etiam facilisis quam in lacus viverra, nec euismod
+      nunc vestibulum. Sed ac venenatis augue, at luctus odio. Phasellus urna
+      nunc, volutpat sed augue at, mollis lobortis libero. Vivamus nec tellus
+      non quam feugiat venenatis. Maecenas laoreet tellus eu nisl pretium,
+      quis aliquet libero eleifend. Quisque viverra nunc et mauris ultricies,
+      at porta massa eleifend. Aliquam vitae lorem mattis, lacinia nulla sit
+      amet, commodo sapien.
+    </p>
+  </div>
+</div>
+  <div class="column is-half">
+    <div class="box">
+      <h1>draw?</h1>
+      <hr>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+        sed velit sit amet felis eleifend finibus. Maecenas eu nisl sit amet
+        augue vehicula ultricies. Class aptent taciti sociosqu ad litora
+        torquent per conubia nostra, per inceptos himenaeos. Morbi sit amet
+        lectus at augue vulputate ultrices. Ut volutpat ullamcorper vulputate.
+        Praesent et orci quis nisi consequat eleifend ut sed purus. Donec quis
+        feugiat leo. Ut in posuere velit, sed iaculis augue. Ut vitae libero nec
+        ex posuere facilisis. 
+      </p>
+      <img src="static/186547938_3990255997722098_1786875754172738849_n.jpg">
+    </div>
+  </div>
+  <div class="column is-one-quarter">
+    <div class="box">
+      <h1>Text</h1>
+      <hr>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+        sed velit sit amet felis eleifend finibus. Maecenas eu nisl sit amet
+        augue vehicula ultricies. Class aptent taciti sociosqu ad litora
+        torquent per conubia nostra, per inceptos himenaeos. Morbi sit amet
+        lectus at augue vulputate ultrices. Ut volutpat ullamcorper vulputate.
+        Praesent et orci quis nisi consequat eleifend ut sed purus. Donec quis
+        feugiat leo. Ut in posuere velit, sed iaculis augue. Ut vitae libero nec
+        ex posuere facilisis. Etiam facilisis quam in lacus viverra, nec euismod
+        nunc vestibulum. Sed ac venenatis augue, at luctus odio. Phasellus urna
+        nunc, volutpat sed augue at, mollis lobortis libero. Vivamus nec tellus
+        non quam feugiat venenatis. Maecenas laoreet tellus eu nisl pretium,
+        quis aliquet libero eleifend. Quisque viverra nunc et mauris ultricies,
+        at porta massa eleifend. Aliquam vitae lorem mattis, lacinia nulla sit
+        amet, commodo sapien.
+      </p>
+    </div>
+  </div>
+</div>
 
-<section class="section">
-  <h2 class="subtitle" />
+<style>
+  h1 {
+    font-size: large;
+    font-weight: bold;
+  }
+  .box {
+    padding: 2em;
+    margin-top: 2em;
+  }
 
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed
-    velit sit amet felis eleifend finibus. Maecenas eu nisl sit amet augue
-    vehicula ultricies. Class aptent taciti sociosqu ad litora torquent per
-    conubia nostra, per inceptos himenaeos. Morbi sit amet lectus at augue
-    vulputate ultrices. Ut volutpat ullamcorper vulputate. Praesent et orci quis
-    nisi consequat eleifend ut sed purus. Donec quis feugiat leo. Ut in posuere
-    velit, sed iaculis augue. Ut vitae libero nec ex posuere facilisis. Etiam
-    facilisis quam in lacus viverra, nec euismod nunc vestibulum. Sed ac
-    venenatis augue, at luctus odio. Phasellus urna nunc, volutpat sed augue at,
-    mollis lobortis libero. Vivamus nec tellus non quam feugiat venenatis.
-    Maecenas laoreet tellus eu nisl pretium, quis aliquet libero eleifend.
-    Quisque viverra nunc et mauris ultricies, at porta massa eleifend. Aliquam
-    vitae lorem mattis, lacinia nulla sit amet, commodo sapien.
-  </p>
-</section>
+
+
+</style>
