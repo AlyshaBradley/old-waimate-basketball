261518d162a9df456d83610cb946074057d9b670
AlyshaBradley <bradleal02@craighead.school.nz>
2021-05-18 16:31:46 +1200

    Created and began to style Nav
    ADDED:
    - created navigation component.
    - began styling navbar
    - added links for navigation
    - created all pages

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,21 @@
+<nav class="navbar is-link">
+    <div class="container">
+        <div id="navMenu" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item" href="."> Home </a>
+                <a class="navbar-item" href="about-us.svelte"> About Us/History </a>
+                <a class="navbar-item" href="draws.svelte"> Draws </a>
+                <a class="navbar-item" href="results.svelte"> Results </a>
+                <a class="navbar-item" href="gallery.svelte"> Gallery </a>
+                <a class="navbar-item" href="contact.svelte"> Contact </a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<style>
+    a {
+        padding-left: 3em;
+        padding-right: 3em;
+    }
+</style>
